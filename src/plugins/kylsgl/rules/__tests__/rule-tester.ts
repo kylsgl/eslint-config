b987ac441b2275e5387a995a4bd3b282d08b4e2a
kylsgl <hello@kylsgl.com>
2025-12-10 18:09:32 +0800

    test: configure eslint rule tester to use vitest
    Signed-off-by: kylsgl <hello@kylsgl.com>

--- src/plugins/kylsgl/rules/__tests__/rule-tester.ts
+++ src/plugins/kylsgl/rules/__tests__/rule-tester.ts
@@ -1,4 +1,9 @@
 import { RuleTester } from 'eslint';
+import { describe, it } from 'vitest';
+
+RuleTester.describe = describe;
+RuleTester.it = it;
+RuleTester.itOnly = it.only;
 
 const ruleTester = new RuleTester();
 
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		coverage: {
-			all: false,
 			provider: 'istanbul',
 		},
 		environment: 'node',
-		globals: true,
+		include: ['./src/**/*.{test,spec}.{ts,tsx}'],
 		reporters: 'verbose',
 		testTimeout: 15e3,
 		watch: false,
